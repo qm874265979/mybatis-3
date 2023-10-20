5dcad6ab02be63d4786d4042ef436e5e6cf6f9e1
QM <qm874265979@163.com>
2023-10-20 17:19:59 +0800

    ------Invoker调用者接口，以及获取属性调用器、设置属性调用者器、指定方法调用器等实现类。

--- src/main/java/org/apache/ibatis/reflection/invoker/AmbiguousMethodInvoker.java
+++ src/main/java/org/apache/ibatis/reflection/invoker/AmbiguousMethodInvoker.java
@@ -21,6 +21,9 @@ import java.lang.reflect.Method;
 
 import org.apache.ibatis.reflection.ReflectionException;
 
+/**
+ *
+ */
 public class AmbiguousMethodInvoker extends MethodInvoker {
   private final String exceptionMessage;
 
