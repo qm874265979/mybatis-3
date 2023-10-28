fdae117c35f87b45e75d580430810001c5d4438c
QM <qm874265979@163.com>
2023-10-28 14:46:26 +0800

    ------FifoCache：基于先进先出的淘汰机制的 Cache 实现类。《装饰器模式》

--- src/main/java/org/apache/ibatis/cache/decorators/FifoCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/FifoCache.java
@@ -21,19 +21,29 @@ import java.util.LinkedList;
 import org.apache.ibatis.cache.Cache;
 
 /**
+ * 基于先进先出的淘汰机制的 Cache 实现类
  * FIFO (first in, first out) cache decorator.
  *
  * @author Clinton Begin
  */
 public class FifoCache implements Cache {
 
+  /**
+   * 装饰的 Cache 对象
+   */
   private final Cache delegate;
+  /**
+   * 双端队列，记录缓存键的添加
+   */
   private final Deque<Object> keyList;
+  /**
+   * 队列上限
+   */
   private int size;
 
   public FifoCache(Cache delegate) {
     this.delegate = delegate;
-    this.keyList = new LinkedList<>();
+    this.keyList = new LinkedList<>();// 使用了 LinkedList
     this.size = 1024;
   }
 
@@ -53,6 +63,7 @@ public class FifoCache implements Cache {
 
   @Override
   public void putObject(Object key, Object value) {
+    // 循环 keyList
     cycleKeyList(key);
     delegate.putObject(key, value);
   }
@@ -64,6 +75,7 @@ public class FifoCache implements Cache {
 
   @Override
   public Object removeObject(Object key) {
+    // <2>问题：在移除指定缓存时，不会移除 keyList 里占用的一个名额。
     return delegate.removeObject(key);
   }
 
@@ -74,7 +86,9 @@ public class FifoCache implements Cache {
   }
 
   private void cycleKeyList(Object key) {
+    // <1> 添加到 keyList 对位。问题：如果重复添加一个缓存，那么在 keyList 里会存储两个，占用了缓存上限的两个名额。
     keyList.addLast(key);
+    // 超过上限，将队首位移除
     if (keyList.size() > size) {
       Object oldestKey = keyList.removeFirst();
       delegate.removeObject(oldestKey);
