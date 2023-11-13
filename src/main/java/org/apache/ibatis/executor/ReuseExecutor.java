c9d4dd7060f0a721bdf6b5a39e3854832b96d4f7
QM <qm874265979@163.com>
2023-11-13 13:23:24 +0800

    ------ReuseExecutor：继承 BaseExecutor 抽象类，可重用的 Executor 实现类。每次开始读或写操作，优先从缓存中获取对应的 Statement 对象。如果不存在，才进行创建。执行完成后，不关闭该 Statement 对象。

--- src/main/java/org/apache/ibatis/executor/ReuseExecutor.java
+++ src/main/java/org/apache/ibatis/executor/ReuseExecutor.java
@@ -33,11 +33,18 @@ import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.transaction.Transaction;
 
-/**
+/** 继承 BaseExecutor 抽象类，可重用的 Executor 实现类
+ *  -每次开始读或写操作，优先从缓存中获取对应的 Statement 对象。如果不存在，才进行创建。
+ *  -执行完成后，不关闭该 Statement 对象。
+ *  -其它的，和 SimpleExecutor 是一致的。
  * @author Clinton Begin
  */
 public class ReuseExecutor extends BaseExecutor {
 
+  /**
+   * Statement 的缓存
+   * KEY ：SQL
+   */
   private final Map<String, Statement> statementMap = new HashMap<>();
 
   public ReuseExecutor(Configuration configuration, Transaction transaction) {
@@ -47,33 +54,49 @@ public class ReuseExecutor extends BaseExecutor {
   @Override
   public int doUpdate(MappedStatement ms, Object parameter) throws SQLException {
     Configuration configuration = ms.getConfiguration();
+    // 创建 StatementHandler 对象
     StatementHandler handler = configuration.newStatementHandler(this, ms, parameter, RowBounds.DEFAULT, null, null);
+    // 初始化 StatementHandler 对象
     Statement stmt = prepareStatement(handler, ms.getStatementLog());
+    // 执行 StatementHandler  ，进行写操作
     return handler.update(stmt);
   }
 
   @Override
   public <E> List<E> doQuery(MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
     Configuration configuration = ms.getConfiguration();
+    // 创建 StatementHandler 对象
     StatementHandler handler = configuration.newStatementHandler(wrapper, ms, parameter, rowBounds, resultHandler, boundSql);
+    // <1> 调用 #prepareStatement(StatementHandler handler, Log statementLog) 方法，初始化 StatementHandler 对象
     Statement stmt = prepareStatement(handler, ms.getStatementLog());
+    // 执行 StatementHandler  ，进行读操作
     return handler.query(stmt, resultHandler);
   }
 
   @Override
   protected <E> Cursor<E> doQueryCursor(MappedStatement ms, Object parameter, RowBounds rowBounds, BoundSql boundSql) throws SQLException {
     Configuration configuration = ms.getConfiguration();
+    // 创建 StatementHandler 对象
     StatementHandler handler = configuration.newStatementHandler(wrapper, ms, parameter, rowBounds, null, boundSql);
+    // 初始化 StatementHandler 对象
     Statement stmt = prepareStatement(handler, ms.getStatementLog());
+    // 执行 StatementHandler  ，进行读操作
     return handler.queryCursor(stmt);
   }
 
+  /**
+   * ReuseExecutor 考虑到重用性，但是 Statement 最终还是需要有地方关闭。答案就在 #doFlushStatements(boolean isRollback) 方法中。
+   * 而 BaseExecutor 在关闭 #close() 方法中，最终也会调用该方法，从而完成关闭缓存的 Statement 对象们
+   * 另外，BaseExecutor 在提交或者回滚事务方法中，最终也会调用该方法，也能完成关闭缓存的 Statement 对象们
+   */
   @Override
   public List<BatchResult> doFlushStatements(boolean isRollback) {
+    // 关闭缓存的 Statement 对象们
     for (Statement stmt : statementMap.values()) {
       closeStatement(stmt);
     }
     statementMap.clear();
+    // 返回空集合
     return Collections.emptyList();
   }
 
@@ -81,14 +104,22 @@ public class ReuseExecutor extends BaseExecutor {
     Statement stmt;
     BoundSql boundSql = handler.getBoundSql();
     String sql = boundSql.getSql();
+    // 调用 #hasStatementFor(String sql) 方法，判断是否存在对应的 Statement 对象
     if (hasStatementFor(sql)) {
+      // <1.1> 调用 #getStatement(String s) 方法，从缓存中获得 Statement 或 PrepareStatement 对象
       stmt = getStatement(sql);
+      // <1.2> 调用 #applyTransactionTimeout(Statement stmt) 方法，设置事务超时时间
       applyTransactionTimeout(stmt);
+      // 不存在
     } else {
+      // <2.1> 获得 Connection 对象
       Connection connection = getConnection(statementLog);
+      // <2.2> 创建 Statement 或 PrepareStatement 对象
       stmt = handler.prepare(connection, transaction.getTimeout());
+      // <2.3> 添加到缓存中
       putStatement(sql, stmt);
     }
+    // <2> 调用 StatementHandler#prepare(Statement statement) 方法，设置 SQL 上的参数，例如 PrepareStatement 对象上的占位符
     handler.parameterize(stmt);
     return stmt;
   }
@@ -96,7 +127,7 @@ public class ReuseExecutor extends BaseExecutor {
   private boolean hasStatementFor(String sql) {
     try {
       Statement statement = statementMap.get(sql);
-      return statement != null && !statement.getConnection().isClosed();
+      return statement != null && !statement.getConnection().isClosed();//要求连接未关闭。
     } catch (SQLException e) {
       return false;
     }
