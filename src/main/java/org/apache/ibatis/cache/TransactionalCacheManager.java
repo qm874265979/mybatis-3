40b80192152c026e0a8a4d4ee5ea7c01683fa05c
QM <qm874265979@163.com>
2023-11-14 11:21:46 +0800

    ------TransactionalCacheManager：TransactionalCache 管理器。

--- src/main/java/org/apache/ibatis/cache/TransactionalCacheManager.java
+++ src/main/java/org/apache/ibatis/cache/TransactionalCacheManager.java
@@ -21,10 +21,16 @@ import java.util.Map;
 import org.apache.ibatis.cache.decorators.TransactionalCache;
 
 /**
+ * TransactionalCache 管理器
  * @author Clinton Begin
  */
 public class TransactionalCacheManager {
 
+  /**
+   * Cache 和 TransactionalCache 的映射
+   * 为什么是一个 Map 对象呢？
+   *  -因为在一次的事务过程中，可能有多个不同的 MappedStatement 操作，而它们可能对应多个 Cache 对象
+   */
   private final Map<Cache, TransactionalCache> transactionalCaches = new HashMap<>();
 
   public void clear(Cache cache) {
@@ -32,13 +38,19 @@ public class TransactionalCacheManager {
   }
 
   public Object getObject(Cache cache, CacheKey key) {
+    // 首先，获得 Cache 对应的 TransactionalCache 对象
+    // 然后从 TransactionalCache 对象中，获得 key 对应的值
     return getTransactionalCache(cache).getObject(key);
   }
 
   public void putObject(Cache cache, CacheKey key, Object value) {
+    // 首先，获得 Cache 对应的 TransactionalCache 对象
+    // 然后，添加 KV 到 TransactionalCache 对象中
     getTransactionalCache(cache).putObject(key, value);
   }
 
+  //提交所有 TransactionalCache
+  //通过调用该方法，TransactionalCache 存储的当前事务的缓存，会同步到其对应的 Cache 对象
   public void commit() {
     for (TransactionalCache txCache : transactionalCaches.values()) {
       txCache.commit();
@@ -52,6 +64,8 @@ public class TransactionalCacheManager {
   }
 
   private TransactionalCache getTransactionalCache(Cache cache) {
+    //从 transactionalCaches 获得 Cache 对象，对应的 TransactionalCache 对象。
+    //如果不存在，则创建一个 TransactionalCache 对象，并添加到 transactionalCaches 中
     return transactionalCaches.computeIfAbsent(cache, TransactionalCache::new);
   }
 
