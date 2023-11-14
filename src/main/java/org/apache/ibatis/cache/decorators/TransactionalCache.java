f614eab7aa008d9aac6917ab44df242e5a980a39
QM <qm874265979@163.com>
2023-11-14 11:35:46 +0800

    ------TransactionalCache ，实现 Cache 接口，支持事务的 Cache 实现类，主要用于二级缓存中。

--- src/main/java/org/apache/ibatis/cache/decorators/TransactionalCache.java
+++ src/main/java/org/apache/ibatis/cache/decorators/TransactionalCache.java
@@ -25,6 +25,7 @@ import org.apache.ibatis.logging.Log;
 import org.apache.ibatis.logging.LogFactory;
 
 /**
+ * <p>实现 Cache 接口，支持事务的 Cache 实现类，主要用于二级缓存中</p>
  * The 2nd level cache transactional buffer.
  * <p>
  * This class holds all cache entries that are to be added to the 2nd level cache during a Session.
@@ -39,9 +40,26 @@ public class TransactionalCache implements Cache {
 
   private static final Log log = LogFactory.getLog(TransactionalCache.class);
 
+  /**
+   * 委托的 Cache 对象。
+   * 实际上，就是二级缓存 Cache 对象。
+   */
   private final Cache delegate;
+  /**
+   * 提交时，清空 {@link #delegate}
+   *
+   * 初始时，该值为 false
+   * 清理后{@link #clear()} 时，该值为 true ，表示持续处于清空状态
+   */
   private boolean clearOnCommit;
+  /**
+   * 在事务未提交时，entriesToAddOnCommit 属性，会暂存当前事务新产生的缓存 KV 对
+   * 在事务提交时，entriesToAddOnCommit 属性，会同步到二级缓存 delegate 中
+   */
   private final Map<Object, Object> entriesToAddOnCommit;
+  /**
+   * 查找不到的 KEY 集合
+   */
   private final Set<Object> entriesMissedInCache;
 
   public TransactionalCache(Cache delegate) {
@@ -64,13 +82,19 @@ public class TransactionalCache implements Cache {
   @Override
   public Object getObject(Object key) {
     // issue #116
+    // <1> 从 delegate 中获取 key 对应的 value
     Object object = delegate.getObject(key);
+    // <2> 如果不存在，则添加到 entriesMissedInCache 中。这是个神奇的逻辑？？？答案见 commit() 和 #rollback() 方法
     if (object == null) {
       entriesMissedInCache.add(key);
     }
     // issue #146
+    // <3> 如果 clearOnCommit 为 true ，表示处于持续清空状态，则返回 null 。
+    // 因为在事务未结束前，我们执行的清空缓存操作不好同步到 delegate 中，所以只好通过 clearOnCommit 来标记处于清空状态。
+    // 那么，如果处于该状态，自然就不能返回 delegate 中查找的结果
     if (clearOnCommit) {
       return null;
+      // <4> 返回 value
     } else {
       return object;
     }
@@ -78,43 +102,62 @@ public class TransactionalCache implements Cache {
 
   @Override
   public void putObject(Object key, Object object) {
+    // 暂存 KV 到 entriesToAddOnCommit 中
     entriesToAddOnCommit.put(key, object);
   }
 
   @Override
   public Object removeObject(Object key) {
+    // 不太明白为什么是这样的实现。不过目前也暂时不存在调用该方法的情况。暂时忽略
     return null;
   }
 
+  //该方法，不会清空 delegate 的缓存。真正的清空，在事务提交时
   @Override
   public void clear() {
+    // <1> 标记 clearOnCommit 为 true
     clearOnCommit = true;
+    // <2> 清空 entriesToAddOnCommit
     entriesToAddOnCommit.clear();
   }
 
   public void commit() {
+    // <1> 如果 clearOnCommit 为 true ，则清空 delegate 缓存
     if (clearOnCommit) {
       delegate.clear();
     }
+    // 将 entriesToAddOnCommit、entriesMissedInCache 刷入 delegate 中
     flushPendingEntries();
+    // 重置
     reset();
   }
 
   public void rollback() {
+    // <1> 调用 #unlockMissedEntries() 方法，将 entriesMissedInCache 同步到 delegate 中
     unlockMissedEntries();
+    // <2> 重置
     reset();
   }
 
+  // 因为，一个 Executor 可以提交多次事务，而 TransactionalCache 需要被重用，那么就需要重置回初始状态
   private void reset() {
+    // 重置 clearOnCommit 为 false
     clearOnCommit = false;
+    // 清空 entriesToAddOnCommit、entriesMissedInCache
     entriesToAddOnCommit.clear();
     entriesMissedInCache.clear();
   }
 
+  /**
+   * 疑惑 entriesMissedInCache 同步到 delegate 中，会不会存在问题。因为当前事务未查找到，不代表其他事务恰好实际能查到。这样，岂不是会将缓存错误的置空。
+   * 后来一想，缓存即使真的被错误的置空，最多也就多从数据库中查询一次罢了
+   */
   private void flushPendingEntries() {
+    // 将 entriesToAddOnCommit 刷入 delegate 中
     for (Map.Entry<Object, Object> entry : entriesToAddOnCommit.entrySet()) {
       delegate.putObject(entry.getKey(), entry.getValue());
     }
+    // 将 entriesMissedInCache 刷入 delegate 中
     for (Object entry : entriesMissedInCache) {
       if (!entriesToAddOnCommit.containsKey(entry)) {
         delegate.putObject(entry, null);
@@ -122,6 +165,7 @@ public class TransactionalCache implements Cache {
     }
   }
 
+  // 即使事务回滚，也不妨碍在事务的执行过程中，发现 entriesMissedInCache 不存在对应的缓存
   private void unlockMissedEntries() {
     for (Object entry : entriesMissedInCache) {
       try {
